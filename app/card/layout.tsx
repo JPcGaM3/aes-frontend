37c98475fd7d2cef2facb77f38835d5b5269d505
JPcGam3 <jaitniphat@gmail.com>
2025-06-11 17:25:23 +0700

    fix: add card back

---
+++ app/card/layout.tsx
@@ -0,0 +1,13 @@
+export default function LoginLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <section className="flex flex-col justify-center items-center gap-4 py-8 md:py-10">
+      <div className="inline-block justify-center w-full text-center">
+        {children}
+      </div>
+    </section>
+  );
+}

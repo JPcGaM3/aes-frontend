af326cfe1e8adb8f86f9242f8b9614e9ee5c9b69
JingKritsada <jing.kritsada@gmail.com>
2025-06-12 17:35:59 +0700

    fix: format drawer

--- components/DrawerComponent.tsx
+++ components/DrawerComponent.tsx
@@ -1,72 +1,64 @@
-import { Button } from "@heroui/button";
 import {
   Drawer,
   DrawerContent,
   DrawerHeader,
-  DrawerBody,
-  DrawerFooter,
+  Button,
+  Tooltip,
 } from "@heroui/react";
-import React from "react";
 
-import { ColorType } from "@/types";
-
-interface ActionConfig {
-  key: string;
-  color: ColorType;
-  label: string;
-  onClick?: (item: any) => void;
+interface DrawerComponentProps {
+  isOpen: boolean;
+  onOpenChange: (open: boolean) => void;
 }
 
-export const DrawerComponent = ({
-  children,
+export default function DrawerComponent({
   isOpen,
-  onOpen,
   onOpenChange,
-  onClose,
-  drawerTitle,
-  actions,
-}: {
-  children: React.ReactNode;
-  isOpen: boolean;
-  onOpen: () => void;
-  onOpenChange: () => void;
-  onClose: () => void;
-  drawerTitle: string;
-  actions?: ActionConfig[];
-}) => {
+}: DrawerComponentProps) {
   return (
     <>
-      <Drawer isOpen={isOpen} onOpenChange={onOpenChange}>
+      <Drawer
+        hideCloseButton
+        backdrop="blur"
+        classNames={{
+          base: "data-[placement=right]:sm:m-2 data-[placement=left]:sm:m-2  rounded-medium",
+        }}
+        isOpen={isOpen}
+        size="xl"
+        onOpenChange={onOpenChange}
+      >
         <DrawerContent>
           {(onClose) => (
             <>
-              <DrawerHeader className="flex flex-col gap-1">
-                {drawerTitle}
-              </DrawerHeader>
-              <DrawerBody>{children}</DrawerBody>
-              <DrawerFooter>
-                <Button color="danger" variant="light" onPress={onClose}>
-                  Close
-                </Button>
-                {actions && actions.length > 0 ? (
-                  actions.map((action) => (
-                    <Button
-                      key={action.key}
-                      color={action.color}
-                      variant="light"
-                      onPress={() => action.onClick && action.onClick(action)}
+              <DrawerHeader className="absolute top-0 inset-x-0 z-50 flex flex-row gap-2 px-2 py-2 border-b border-default-200/50 justify-between bg-content1/50 backdrop-saturate-150 backdrop-blur-lg">
+                <Tooltip content="Close">
+                  <Button
+                    isIconOnly
+                    className="text-default-400"
+                    size="sm"
+                    variant="light"
+                    onPress={onClose}
+                  >
+                    <svg
+                      fill="none"
+                      height="20"
+                      stroke="currentColor"
+                      strokeLinecap="round"
+                      strokeLinejoin="round"
+                      strokeWidth="2"
+                      viewBox="0 0 24 24"
+                      width="20"
+                      xmlns="http://www.w3.org/2000/svg"
                     >
-                      {action.label}
-                    </Button>
-                  ))
-                ) : (
-                  <></>
-                )}
-              </DrawerFooter>
+                      <path d="m13 17 5-5-5-5M6 17l5-5-5-5" />
+                    </svg>
+                  </Button>
+                </Tooltip>
+              </DrawerHeader>
             </>
           )}
         </DrawerContent>
       </Drawer>
     </>
   );
-};
+}

dc066e66f6f99214261f717b68c66f9ab4597596
JingKritsada <jing.kritsada@gmail.com>
2025-06-23 13:27:46 +0700

    fix: make header flexible for filter

--- components/Header.tsx
+++ components/Header.tsx
@@ -3,12 +3,28 @@ import React from "react";
 interface HeaderProps {
   title: string;
   subtitle?: string;
+  className?: string;
+  children?: React.ReactNode;
 }
 
-const Header: React.FC<HeaderProps> = ({ title, subtitle }) => (
-  <header className="mb-6 w-full text-center">
-    <h2 className="text-2xl font-semibold text-gray-900">{title}</h2>
-    {subtitle && <p className="mt-1 text-base text-gray-600">{subtitle}</p>}
+const Header: React.FC<HeaderProps> = ({
+  title,
+  subtitle,
+  className = "mb-6 w-full text-center flex flex-col",
+  children,
+}) => (
+  <header className={className}>
+    <div className="flex flex-row justify-between align-middle w-full">
+      <div className="w-full flex flex-col">
+        <h2 className="text-2xl font-semibold text-gray-900">{title}</h2>
+        {subtitle && <p className="mt-1 text-base text-gray-600">{subtitle}</p>}
+      </div>
+
+      <div className="flex flex-row justify-center items-center gap-2">
+        {children}
+      </div>
+    </div>
+
     <hr className="mt-4 border-gray-200" />
   </header>
 );

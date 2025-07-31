3ad4c3289671db183f2cd7119d2d43ed14f156c7
JPcGam3 <jaitniphat@gmail.com>
2025-07-31 10:24:53 +0700

    test: add logs

--- libs/httpClient.ts
+++ libs/httpClient.ts
@@ -45,6 +45,7 @@ class HttpClient {
 			return response.data;
 		} catch (error: any) {
 			if (axios.isAxiosError(error)) {
+				console.log(error);
 				throw {
 					status: error.response?.status,
 					message: error.response?.data?.message || error.message,
